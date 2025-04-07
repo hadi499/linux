588bcdc51838d1668e6904f18de6460e571468a0
hadi499 <57480038+hadi499@users.noreply.github.com>
2025-04-07 09:17:24 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -13,4 +13,8 @@ fish_add_path /home/hadi/golang/bin
 set -x GOPATH /home/hadi/golang
 
 set -U fish_user_paths /home/hadi/dev/tools/jdk-15.0.2/bin
+#untuk nvm
 set -gx NVM_DIR $HOME/.nvm
+
+# Load fnm automatically
+fnm env --use-on-cd | source
